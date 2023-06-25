d10e26ad21139a94da426801ba1053cfe0fadac5
AnaM <test>
2023-06-25 15:11:33 -0500

    v3 test carrito compras

---
+++ CarritoCompras/test/carrito.test.ts
@@ -0,0 +1,14 @@
+import request from 'supertest'
+import app from '../src/app'
+
+test(
+    'Debe obtener un codigo de respuesta exitoso',
+    async ()=>{
+        //Hacer la peticion
+        const response = await request(app).get('/4')
+
+        //DEfinir las afirmaciones
+        expect(response.statusCode).toBe(200)
+    }
+)
+
